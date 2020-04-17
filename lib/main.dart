56df5d2f5ac51130069ce92c4734ab336fcf1ba9
ratchasak <ratchasak@gmail.com>
2020-04-17 23:47:27 +0700

    d

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Startup Name Generator',
       theme: ThemeData(
         primaryColor: Colors.white,
